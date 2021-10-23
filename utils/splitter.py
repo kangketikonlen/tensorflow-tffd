bcd7764a3f790bc0bcf91902c6846937d3ce3333
Gilang Pratama <pratamapriadi96@gmail.com>
2021-10-24 05:20:33 +0700

    :zap: update splitter & remove requirement txt

--- utils/splitter.py
+++ utils/splitter.py
@@ -2,17 +2,32 @@ import cv2
 import os
 
 path = os.getcwd()
-filename = "moona.mp4"
+object_name = "moona"
+filename = object_name+".mp4"
 upload_dir = os.path.join(path, "uploads/", filename)
-output_dir = os.path.join(path, "outputs/moona/")
+output_dir = os.path.join(path, "outputs/"+object_name+"/")
 capture = cv2.VideoCapture(upload_dir)
 frameNr = 0
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
 while (True):
     success, frame = capture.read()
 
     if success:
-        cv2.imwrite(f'{output_dir}frame_{frameNr}.jpg', frame)
+        original = f'{output_dir}{object_name}_{frameNr}.jpg'
+        cv2.imwrite(original, frame)
+        resize = cv2.imread(original, cv2.IMREAD_UNCHANGED)
+        scale_percent = 40  # percent of original size
+        width = int(resize.shape[1] * scale_percent / 100)
+        height = int(resize.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized = cv2.resize(resize, dim, interpolation=cv2.INTER_AREA)
+        cv2.imwrite(original, resized)
+        print('File created :', original)
     else:
         break
+
     frameNr = frameNr+1
 capture.release()
